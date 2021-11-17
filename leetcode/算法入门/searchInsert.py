2e69793ac152d8104cf82a31c632214d67b916bb
wjm <18717545422@163.com>
2021-11-17 10:50:26 +0800

    搜索插入位置

---
+++ leetcode/算法入门/searchInsert.py
@@ -0,0 +1,18 @@
+from typing import List
+
+"""
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+请必须使用时间复杂度为 O(log n) 的算法。
+"""
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0,len(nums)-1
+        while left <= right:
+            middle = left + (right - left) // 2
+            if nums[middle] <target:
+                left = middle + 1
+            elif nums[middle] > target:
+                right = middle - 1
+            else:
+                return middle
+        return right + 1
