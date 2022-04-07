c78e199ad92dc04fce903a45a092dd307b5cf832
wangjiameng <wangjiameng01@baidu.com>
2022-04-07 13:22:55 +0800

    python 的print

---
+++ jichu/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@Time : 2022/4/6 8:33 PM 
+@File  : __init__.py.py
+@Author:  wangjiameng01
+@Desc  :
+"""
---
+++ jichu/print_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@Time : 2022/4/6 8:33 PM 
+@File  : print_test.py
+@Author:  wangjiameng01
+@Desc  :
+"""
+a = 100
+b = 50
+print(a+b)
+print(a*b)
+print("北京欢迎你")
+print(a, b, "北京欢迎你")
+print('b')
+print(chr(98))
